ccb1b5ea1877de14119378fa4732b128a0e0cfd6
mewchew <anastasia.victor@gmail.com>
2014-12-29 16:48:32 -0800

    got carousel template working

--- app.js
+++ app.js
@@ -2,28 +2,22 @@
  * Module dependencies
  */
 var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
   
 var app = express()
-	function compile(str, path) {
-		return stylus(str)
-		.set('filename', path)
-		.use(nib())
-	}
-app.set('views', __dirname + '/views')
+
+app.set('views', __dirname + '/client/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/client'))
 
 app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
+  res.render('carousel',
+  { pathToAssets : '/bower_components/bootstrap',
+  pathToSelectedTemplateWithinBootstrap : '/stylesheets',
+  pathToImages : '/img'
+   }
   )
 })
+
+console.log("Listening on port 3000...")
 app.listen(3000)
