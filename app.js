b3d1e613e22e62825e705f58585f7a9e612f8e17
mewchew <anastasia.victor@gmail.com>
2014-12-29 20:11:48 -0800

    added partials and yaml

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /*
  * Module dependencies
  */
-var express = require('express')
+var express = require('express'),
+	YAML = require('yamljs')
   
 var app = express()
 
@@ -10,11 +11,20 @@ app.set('view engine', 'jade')
 app.use(express.logger('dev'))
 app.use(express.static(__dirname + '/client'))
 
+function loadInfo (dir) {
+	file = dir + ("/main.yml")
+	info = YAML.load(file)
+	console.log("\n" + dir +"\n")
+	console.log(JSON.stringify(info,undefined,2));
+	return info
+}
+
 app.get('/', function (req, res) {
-  res.render('carousel',
-  { pathToAssets : '/bower_components/bootstrap',
-  pathToSelectedTemplateWithinBootstrap : '/stylesheets',
-  pathToImages : '/img'
+  res.render('carousel.jade', { 
+  	info : loadInfo ( __dirname + '/client/content'),
+  	pathToAssets : '/bower_components/bootstrap',
+	pathToSelectedTemplateWithinBootstrap : '/stylesheets',
+	pathToImages : '/content/img'
    }
   )
 })
