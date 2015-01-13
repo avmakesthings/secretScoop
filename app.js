95dadd826b2f17d8ca872392e91ebc05f057fc77
mewchew <anastasia.victor@gmail.com>
2015-01-13 08:31:05 -0800

    Split up jade files, restructured templating, modified css and less properties in carousel.less

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var express = require('express'),
 	YAML = require('yamljs')
   
 var app = express()
+var mainInfo = loadInfo ( __dirname + '/client/content/main.yml');
+var flavorInfo = loadInfo ( __dirname + '/client/content/flavors.yml');
+var pressInfo = loadInfo ( __dirname + '/client/content/press.yml');
 
 app.set('views', __dirname + '/client/views')
 app.set('view engine', 'jade')
@@ -18,16 +21,41 @@ function loadInfo (file) {
 	return info
 }
 
-app.get('/', function (req, res) {
-  res.render('carousel.jade', { 
-  	mainInfo : loadInfo ( __dirname + '/client/content/main.yml'),
-  	flavorInfo : loadInfo ( __dirname + '/client/content/flavors.yml'),
+var render = function (req, res, template) {
+  	res.render(template, { 
+  	_mainInfo : mainInfo,
+  	_flavorInfo : flavorInfo,
+  	_pressInfo : pressInfo,
   	pathToAssets : '/bower_components/bootstrap',
 	pathToSelectedTemplateWithinBootstrap : '/stylesheets',
 	pathToImages : '/content/img'
-   }
-  )
+	})
+}
+
+app.get('/', function (req, res) { 
+	render (req, res, "index.jade")
 })
+
+app.get('/about', function (req, res) { 
+	render (req, res, "about.jade")
+})
+
+app.get('/flavors', function (req, res) { 
+	render (req, res, "flavors.jade")
+})
+
+app.get('/order', function (req, res) { 
+	render (req, res, "order.jade")
+})
+
+app.get('/events', function (req, res) { 
+	render (req, res, "events.jade")
+})
+
+app.get('/press', function (req, res) { 
+	render (req, res, "press.jade")
+})
+
 
 console.log("Listening on port 3000...")
 app.listen(3000)
